981215b7f14570aeaf41c8ced4716f1674b42fd2
halanjoel11 <130591060+halanjoel11@users.noreply.github.com>
2023-07-02 23:16:12 -0500

    Add files via upload

---
+++ caracteres.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+
+    char j1,j2,j3;
+    printf("Intruduzca e caracteres\n");
+    scanf("%c %c %c",&j1,&j2,&j3);
+    printf("%c-%c-%c",j1,j2,j3);
+    return 0;
+}
---
+++ holajoel.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hola profe ando haciendo udemi\n");
+    return 0;
+}
